0926e04a0ff69b773d0717342727809fd48f4e1d
AnjaliK-123 <anjalikhadake888@gmail.com>
2025-01-03 19:16:41 -0800

    Add basic structure

---
+++ script.js
@@ -0,0 +1,41 @@
+class calculator {
+    constructor() {
+        // getting the dom elements 
+        globalThis.display = document.querySelector("#display");
+        // intializing calculator state 
+        this.currentoperand = "";
+        this.perviousopernad = "";
+        this.operation = "";
+        this.shouldReset = false;
+    }
+    // number input logic 
+    appendNumber(nuber) {
+        
+    }
+
+    // handle operations 
+    chooseOperation(operator) {
+        
+    }
+
+    // compute logic to perform calculations
+    compute() {
+        
+    }
+    // display logic 
+    updateDisplay() {
+        
+    }
+    // clear logic 
+    clear() {
+        
+    }
+    // initialize  calculator 
+    const calculator = new Calculator();
+
+    function initializeCalculators() {
+        
+    }
+
+
+}
